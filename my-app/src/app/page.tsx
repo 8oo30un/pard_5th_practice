6ad5b704850f00da825ee2b631e4af88bd6c110b
woo-hyun <kwh77974481@gmail.com>
2025-02-28 12:58:14 +0900

    add: 5th seminar 자료 공부

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,9 +1,18 @@
 // import Image from "next/image";
 "use client";
 
+import axios from "axios";
 // import { useState } from "react";
-import { Heart } from "lucide-react";
+// import { Heart } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+interface Data {
+  name: string;
+  age: number;
+  position: string;
+}
 
 export default function Home() {
   // const name: string = "Kim woo hyun";
@@ -11,44 +20,70 @@ export default function Home() {
   // const mbti: string = "INFP";
   // const love: boolean = false;
 
-  interface User {
-    age: number;
-    name: string;
-    mbti: string;
-    love: boolean;
-  }
+  // interface User {
+  //   age: number;
+  //   name: string;
+  //   mbti: string;
+  //   love: boolean;
+  // }
 
-  interface Pard extends User {
-    // position: string; // developer
-    // generation: number;
-    // role: string; //pardi, pardo, manager
-    [key: string]: string | number | boolean | string[];
-  }
+  const [data, setData] = useState<Data[]>([]);
+  // const [data, setData] = useState<Record<string, Data>>({}); // 객체로 변경
+  // 이렇게 하면 data["woohyun"] 가능
+  const [error, setError] = useState<string | null>(null);
 
-  const PardMember: Pard = {
-    name: "KWH",
-    age: 24,
-    mbti: "INFP",
-    love: false,
-    hobby: ["coding", "music", "weight"],
-    postion: "WEB",
-  };
+  useEffect(() => {
+    axios
+      .get<{ message: string; data: Data[] }>("/sample.json")
+      .then((response) => {
+        setData(response.data.data);
 
-  interface aboutMe<T> {
-    etc: T;
-  }
+        // data 배열을 name을 키로 하는 객체로 변환
+        // const dataObject = response.data.data.reduce((acc, current) => {
+        //   acc[current.name] = current;
+        //   return acc;
+        // }, {} as Record<string, Data>);
 
-  const genericDate: aboutMe<number> = { etc: 20250227 };
-  const genericSeminar: aboutMe<string> = { etc: "pard_5th_1th_seminar" };
+        // setData(dataObject); // 변환된 객체를 상태에 저장
+      })
+      .catch(() => {
+        setError("데이터 가져오는데 실패");
+      });
+  }, []);
 
-  const user: User = { name: "KWH", age: 24, mbti: "INFP", love: false };
+  // interface Pard extends User {
+  //   // position: string; // developer
+  //   // generation: number;
+  //   // role: string; //pardi, pardo, manager
+  //   [key: string]: string | number | boolean | string[];
+  // }
+
+  // const PardMember: Pard = {
+  //   name: "KWH",
+  //   age: 24,
+  //   mbti: "INFP",
+  //   love: false,
+  //   hobby: ["coding", "music", "weight"],
+  //   position: "WEB",
+  //   generation: 5,
+  //   role: "manager",
+  // };
+
+  // interface aboutMe<T> {
+  //   etc: T;
+  // }
+
+  // const genericDate: aboutMe<number> = { etc: 20250227 };
+  // const genericSeminar: aboutMe<string> = { etc: "pard_5th_1th_seminar" };
+
+  // const user: User = { name: "KWH", age: 24, mbti: "INFP", love: false };
   // const pard: User = { name: "Pard", age: 5, mbti: "SEXY", love: true }; // 옆사람 꺼 공유해서 추가하기
 
-  const getUserInfo = (user: User): string => {
-    return `${user.name}은(는) ${user.age}살이며 MBTI는 ${user.mbti}입니다. ${
-      user.love ? "사랑에 빠져 있습니다." : "현재 솔로입니다."
-    }`;
-  };
+  // const getUserInfo = (user: User): string => {
+  //   return `${user.name}은(는) ${user.age}살이며 MBTI는 ${user.mbti}입니다. ${
+  //     user.love ? "사랑에 빠져 있습니다." : "현재 솔로입니다."
+  //   }`;
+  // };
 
   // const PardMember: Pard = {
   //   name: "KWH",
@@ -60,58 +95,72 @@ export default function Home() {
   //   role: "manager",
   // };
 
-  const pardWeb = <T extends User, K extends keyof T>(
-    member: T,
-    key: K
-  ): T[K] => {
-    return member[key];
-  };
+  // type User = {
+  //   age: number;
+  //   name: string;
+  //   mbti: string;
+  //   love: boolean;
+  // }
 
-  const chaechae = { age: 24, name: "chaechae", mbti: "INFP", love: false };
+  // const pardWeb = <T extends User, K extends keyof T>(
+  //   member: T,
+  //   key: K
+  // ): T[K] => {
+  //   return member[key];
+  // };
 
-  const getPardInfo = (user: Pard): string => {
-    return `${user.name}은(는) ${user.age}살이며 MBTI는 ${user.mbti}입니다. ${
-      user.love ? "사랑에 빠져 있습니다." : "현재 솔로입니다."
-    } 파드에서 ${user.position}을 맡고 있고, ${
-      user.generation
-    }기에 참여했고, 역할은 ${user.role}입니다.`;
-  };
+  // const chaechae = { age: 24, name: "chaechae", mbti: "INFP", love: false };
+
+  // const getPardInfo = (user: Pard): string => {
+  //   return `${user.name}은(는) ${user.age}살이며 MBTI는 ${user.mbti}입니다. ${
+  //     user.love ? "사랑에 빠져 있습니다." : "현재 솔로입니다."
+  //   } 파드에서 ${user.position}을 맡고 있고, ${
+  //     user.generation
+  //   }기에 참여했고, 역할은 ${user.role}입니다.`;
+  // };
 
   return (
     <div className=" bg bg-[#D5FFE4] w-screen h-screen ">
       <div className="font-serif text-center bg bg-[#a084e8]">
-        pard_5th_ot_{user.name}
+        {/* pard_5th_ot_{user.name} */}
+        {error && <p>{error}</p>}
       </div>
 
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex justify-center items-center w-[300px] h-[300px]">
-        <Image
-          src="/KakaoTalk_Photo_2025-02-25-15-16-01.png"
-          alt=""
-          layout="fill"
-        />
+        <Image src="/KakaoTalk_Photo_2025-02-25-15-16-01.png" alt="" fill />
       </div>
       <div className="flex items-center justify-center h-screen">
         <div className="grid grid-cols-2 grid-rows-2  w-[800px] h-[800px]">
-          <div className="bg-[#6F61C0] border-none flex items-center justify-center text-center border-2">
-            {user.age}
+          <div className="bg-[#6F61C0] border-none flex items-center justify-center text-center border-2 rounded-tl-3xl">
+            {/* {user.age} */}
+            <p>{data[0]?.name}</p>
           </div>
-          <div className="bg-[#8BE8E5] border-none flex items-center justify-center text-center border-2">
-            {user.mbti}
+          <div className="bg-[#8BE8E5] border-none flex items-center justify-center text-center border-2 rounded-tr-3xl">
+            {/* {user.mbti} */}
+            {/* {data["김우현"]?.age} */}
+            {data[0]?.age}
           </div>
-          <div className="bg-[#8BE8E5]  border-none flex items-center justify-center text-center border-2">
-            <Heart
+          <div className="bg-[#8BE8E5]  border-none flex items-center justify-center text-center border-2 rounded-bl-3xl">
+            {/* <Heart
               className={`w-10 h-10 stroke-red-500 ${
                 user.love ? "fill-red-500" : "fill-none"
               }`}
-            />
+            /> */}
+            {data[0]?.position}
           </div>
-          <div className="bg-[#6F61C0] border-none flex items-center justify-center text-center border-2">
-            {getUserInfo(user)}
-            {/* {getPardInfo(PardMember)} */}
+          <div className="bg-[#6F61C0] border-none flex items-center justify-center text-center border-2 rounded-br-3xl">
+            {/* {getUserInfo(user)} */}
+            {/* {getPardInfo(PardMember)}
             {PardMember["name"]}
             {genericSeminar.etc}
             {genericDate.etc}
-            {pardWeb(chaechae, "name")}
+            {pardWeb(chaechae, "name")} */}
+            <Link
+              href="/crud"
+              className="px-6 py-3 bg-white text-[#6F61c0] rounded-lg shadow-md hover:bg-white"
+            >
+              🔧 CRUD 페이지로 이동
+            </Link>
           </div>
         </div>
       </div>
